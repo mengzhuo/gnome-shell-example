0d4818d1d24459a825c67df2e9994a1b0b6441f9
Meng ZHuo <mengzhuo1203@gmail.com>
2012-07-16 15:49:34 +0800

    Lesson 2: label show up

--- extension.js
+++ extension.js
@@ -1,8 +1,37 @@
+const St = imports.gi.St;
+const Main = imports.ui.main;
+const PanelMenu = imports.ui.panelMenu;
+
+let metadata,indicator = null;
+
+function ExampleIndicator(){
+    this._init();
+}
+
+ExampleIndicator.prototype = {
+    
+    __proto__: PanelMenu.Button.prototype,
+    
+    _init: function (){
+        global.log('EXAMPLE:Enabled');
+        PanelMenu.Button.prototype._init.call(this, St.Align.END);
+
+        this.label = new St.Label({text:"This is Example"});
+        this.actor.add_actor(this.label);
+        
+        Main.panel._rightBox.add_actor(this.actor);
+    },
+    destroy: function(){
+        global.log('EXAMPLE:Disabled');
+        Main.panel._rightBox.remove_actor(this.actor);
+    }
+}
+
 function enable() {
-    global.log('EXAMPLE:Enabled');
+    indicator = new ExampleIndicator();
 }
 function disable() {
-    global.log('EXAMPLE:Disabled');
+    indicator.destroy();
 }
 function init(metadata) {
     global.log('EXAMPLE:%s Initialized '.format(metadata.uuid));
