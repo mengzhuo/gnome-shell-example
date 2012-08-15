b8d639ae6a99fd1498323415ecb555b8bde1188f
Meng ZHuo <mengzhuo1203@gmail.com>
2012-08-16 00:08:28 +0800

    Lesson_3_Chapter_1

--- extension.js
+++ extension.js
@@ -1,6 +1,7 @@
 const St = imports.gi.St;
 const Main = imports.ui.main;
 const PanelMenu = imports.ui.panelMenu;
+const Gtk = imports.gi.Gtk;
 
 let metadata,indicator = null;
 
@@ -17,7 +18,17 @@ ExampleIndicator.prototype = {
         PanelMenu.Button.prototype._init.call(this, St.Align.END);
 
         this.label = new St.Label({text:"This is Example"});
-        this.actor.add_actor(this.label);
+        this.label.add_style_class_name('example');
+        
+        //this.actor.add_actor(this.label);
+        
+        this.iconTheme = Gtk.IconTheme.get_default();
+        this._icon = new St.Icon({ icon_type: St.IconType.SYMBOLIC,
+                                    style_class: 'popup-menu-icon',
+                                    icon_name: 'face-smile',
+                                    icon_size: 26
+                                    });
+        this.actor.add_actor(this._icon);
         
         Main.panel._rightBox.add_actor(this.actor);
     },
